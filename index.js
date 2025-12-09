da6c64aebef2d1898878cfcb556d9db6f3afc548
AliRaj <aliraj15dev@gmail.com>
2025-12-09 12:13:29 +0600

    added mongodb and ping successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 3000;
@@ -9,9 +10,29 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
-  res.send("LocalCheBazaar Server is Working");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.7suxelv.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+app.get("/", (req, res) => {
+  res.send("LocalChefBazaar Server is Working");
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {}
+}
+run().catch(console.dir);
 
 
 
